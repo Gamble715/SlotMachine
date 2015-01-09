233688cc4d239689d7064bc5ecd8d8b16a2ad1f0
Gamble McAdam <gamble.mcadam@gmail.com>
2015-01-09 10:48:42 -0600

    Initial commit

---
+++ Slot Machine/Slot.swift
@@ -0,0 +1,16 @@
+//
+//  Slot.swift
+//  Slot Machine
+//
+//  Created by Gamble McAdam on 12/17/14.
+//  Copyright (c) 2014 Gamble. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Slot {
+    var value = 0
+    var image = UIImage(named: "Ace")
+    var isRed = true
+}
